d1422207fa428603cf6d17996e7ef0003189acef
HugoSev1 <hugosevrain@gmail.com>
2026-03-01 00:06:54 +0100

    Add row support

--- nonoSolvFunc.py
+++ nonoSolvFunc.py
@@ -60,25 +60,55 @@ def getColumnImage(area_array):
                 break
     xColumn = column_coords[0]
     yColumn = column_coords[1]
-    color = (142, 135, 111)
+    color = (0, 0, 0)
     while scrsh.getpixel((xColumn, yColumn)) != color:
         yColumn += 1
-    color = (42,43,35)
+    color = (42, 43, 35)
     while scrsh.getpixel((xColumn, yColumn)) != color:
         xColumn += 1
     column_coords.append(xColumn)
     column_coords.append(yColumn)
-    
-    column_img = ImageGrab.grab(bbox=(column_coords[0],column_coords[1],column_coords[2],column_coords[3]))
-    column_img.save("digits-test.png")
+
+    column_img = ImageGrab.grab(
+        bbox=(column_coords[0], column_coords[1], column_coords[2], column_coords[3]))
+    column_img.save("column-img.png")
     return column_coords
 
 
+def getRowImage(area_array):
+    row_coords = []
+    color = (248, 236, 194)
+    scrsh = pyautogui.screenshot()
+    for x in range(area_array[0], area_array[2]):
+        for y in range(area_array[1], area_array[3]):
+            if scrsh.getpixel((x, y)) == color:
+                row_coords.append(x)
+                row_coords.append(y)
+                break
+            if len(row_coords) >= 2:
+                break
+    xColumn = row_coords[0]
+    yColumn = row_coords[1]
+    color = (0, 0, 0)
+    while scrsh.getpixel((xColumn, yColumn)) != color:
+        xColumn += 1
+    color = (42, 43, 35)
+    while scrsh.getpixel((xColumn, yColumn)) != color:
+        yColumn += 1
+    row_coords.append(xColumn)
+    row_coords.append(yColumn)
+
+    row_img = ImageGrab.grab(
+        bbox=(row_coords[0], row_coords[1], row_coords[2], row_coords[3]))
+    row_img.save("row-img.png")
+    return row_coords
+
+
 # Function that extracts the numbers shown on the Nonogram
-def extractNumbers():
+def extractColumnNumbers():
     extractingArray = []
-    # Divides the image in 5 chunks, and save them
-    img = cv2.imread("digits-test.png")
+    # Divides the image in 5 chunks
+    img = cv2.imread("column-img.png")
     h, w, c, = img.shape
     n_chunks = 5
     chunk_width = w // n_chunks
@@ -125,6 +155,64 @@ def extractNumbers():
 
             if (best_score > 0.7):
                 digits.append((best_match, y))
+        digits.sort(key=lambda x: x[1])
+        result = [d[0] for d in digits]
+        extractingArray.append(result)
+    return extractingArray
+
+
+# Function that extracts the numbers shown on the Nonogram (but for the rows this time)
+def extractRowNumbers():
+    extractingArray = []
+    # Divides the image in 5 chunks
+    img = cv2.imread("row-img.png")
+    h, w, c, = img.shape
+    n_chunks = 5
+    chunk_height = h // n_chunks
+
+    chunks = []
+
+    for i in range(n_chunks):
+        # Separate into chunks (e.g. 5 chunks for a 5x5 Nonogram)
+        start_y = i * chunk_height
+
+        if i == n_chunks - 1:
+            end_y = h
+        else:
+            end_y = (i + 1) * chunk_height
+
+        chunk = img[start_y:end_y, :]
+        chunks.append(chunk)
+        # showImg(chunk)
+
+        # Detect digits
+        gray = cv2.cvtColor(chunk, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+        contours, _ = cv2.findContours(
+            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        digits = []
+        for cnt in contours:
+            x, y, w2, h2 = cv2.boundingRect(cnt)
+            roi = thresh[y:y+h2, x:x+w2]
+            roi = cv2.resize(roi, (50, 50))
+
+            best_match = None
+            best_score = -1
+
+            for digit in range(1, 5):
+                template = cv2.imread(f"templates\\{digit}.png", 0)
+                _, template = cv2.threshold(
+                    template, 150, 255, cv2.THRESH_BINARY_INV)
+                template = cv2.resize(template, (50, 50))
+                score = cv2.matchTemplate(
+                    roi, template, cv2.TM_CCOEFF_NORMED)[0][0]
+
+                if (score > best_score):
+                    best_score = score
+                    best_match = digit
+
+            if (best_score > 0.7):
+                digits.append((best_match, x))
         digits.sort(key=lambda x: x[1])
         result = [d[0] for d in digits]
         extractingArray.append(result)
--- solver.py
+++ solver.py
@@ -3,9 +3,12 @@ import nonoSolvFunc
 # Getting the coordinates of the blue box around the puzzle
 puzzle_coords = nonoSolvFunc.getPuzzleCoords()
 
-# Get the columns part from here
+# Get the columns digits from here
 column_coords = nonoSolvFunc.getColumnImage(puzzle_coords)
+column_digits = nonoSolvFunc.extractColumnNumbers()
 
 # Read the digits from the columns
-column_digits = nonoSolvFunc.extractNumbers()
+row_coords = nonoSolvFunc.getRowImage(puzzle_coords)
+row_digits = nonoSolvFunc.extractRowNumbers()
 print(column_digits)
+print(row_digits)
